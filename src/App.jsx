b97ad3f2ae777d1dd57b2b44ba41b20dc397b629
Manas Hejmadi <manas2all@outlook.com>
2024-10-17 15:34:06 +0530

    Added Basename & did cleanup for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CodeEditor from "./components/CodeEditor.jsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
+      <Router basename='/codeframe'>
+        <Routes >
           <Route exact path="/" element={<CodeEditor />} />
           <Route exact path="/:lang" element={<CodeEditor />} />
         </Routes>
